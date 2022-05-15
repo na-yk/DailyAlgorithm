1185b2bbacad3c49c1b459783b441b8313076f11
ninghao17 <ninghao17@naver.com>
2022-05-16 01:29:29 +0900

    initial commit

--- JAVA_BASICS/HelloWorld.java
+++ JAVA_BASICS/HelloWorld.java
@@ -1,6 +1,10 @@
+//BOJ #2557 Hello World
+//입출력과 연산자
+
+package basics;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args){
+		System.out.println("Hello World");
 	}
 }
