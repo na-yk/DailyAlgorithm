b4df859418a3b55c7b2a7c663fd6aa10236f56eb
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2023-01-04 18:55:38 +0900

    chore: Move directory

--- BOJ/BASIC_MATH/11653.소인수분해.py
+++ BOJ/BASIC_MATH/11653.소인수분해.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 BOJ#11653 소인수분해
 기본 수학 2
@@ -16,22 +15,3 @@ while n != 1:
     else:
         cur+=1
 
-=======
-"""
-BOJ#11653 소인수분해
-기본 수학 2
-"""
-
-import sys
-
-n = int(sys.stdin.readline().rstrip())
-
-cur = 2
-while n != 1:
-    if (n%cur) == 0:
-        n = n//cur
-        print(cur)    
-    else:
-        cur+=1
-
->>>>>>> 44eb9ab6eb732a550c37acd6f8bafd6100c82fab
